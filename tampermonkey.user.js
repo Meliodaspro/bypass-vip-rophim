bf4e9ebd4fbfafb3275f3fd25c0358d076f53b71
Sơn <41245943+Meliodaspro@users.noreply.github.com>
2026-02-04 17:31:24 +0700

    Update version number to 1.8 and add new match URL

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Rophim Full VIP By US
 // @namespace    http://tampermonkey.net/
-// @version      1.7
+// @version      1.8
 // @description  Bypass VIP + Coin trên Rophim
 // @author       US
 // @require      https://raw.githubusercontent.com/Meliodaspro/bypass-vip-rophim/refs/heads/main/Bypas-vip-rophim.js
 // @match        *://www.rophim.me/*
 // @match        *://www.rophim.mx/*
 // @match        *://www.rophim.li/*
+// @match        *://www.rophim.la/*
 // @run-at       document-start
 // @grant        none
 // @icon         https://avatars.githubusercontent.com/u/120646974?s=48&v=4
